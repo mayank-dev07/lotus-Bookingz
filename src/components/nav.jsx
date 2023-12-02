6bf7410074ee8c71c5141b9a87ffb999fba04434
mayank-dev07 <devmayank.hts@gmail.com>
2023-12-02 17:05:13 +0530

    commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect, useLayoutEffect } from "react";
 import { AlignJustify } from "lucide-react";
 import { Link } from "react-router-dom";
 import instance from "./axios";
-import axios from "axios";
 
 export default function Nav() {
   const navigate = useNavigate();
